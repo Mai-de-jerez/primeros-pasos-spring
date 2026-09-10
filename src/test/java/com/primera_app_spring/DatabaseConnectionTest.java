6fca29e36ea1535fbc0413ede27155a5305481fc
Mai <mainen1985@gmail.com>
2026-09-10 17:09:26 +0200

    probando flujos de trabajo

--- src/test/java/com/primera_app_spring/DatabaseConnectionTest.java
+++ src/test/java/com/primera_app_spring/DatabaseConnectionTest.java
@@ -20,10 +20,10 @@ class DatabaseConnectionTest {
     @Test
     @DisplayName("Debe obtener una conexión válida del pool HikariCP")
     void testConexionBaseDatos() throws SQLException {
-        // Verifica que Spring ha inyectado el DataSource correctamente
+        // para verificar que Spring ha inyectado el DataSource correctamente
         assertNotNull(dataSource, "El DataSource no debería ser nulo");
 
-        // Solicita una conexión al pool HikariCP
+        // solicita una conexión al pool HikariCP
         try (Connection connection = dataSource.getConnection()) {
             assertNotNull(connection, "La conexión a la BD no debería ser nula");
             assertTrue(connection.isValid(2), "La conexión a MySQL debería responder en menos de 2 segundos");
